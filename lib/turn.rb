3fb00ada94738444373c3f06800b1725c056b6ac
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-01-29 16:54:12 -0700

    Iteration 1 Turn

---
+++ lib/turn.rb
@@ -0,0 +1,15 @@
+class Turn
+  attr_reader :guess, :card
+  def initialize(guess, card)
+    @guess = guess
+    @card = card
+  end
+
+  def feedback
+    if @guess = card.answer
+      p "Correct!"
+    else
+      p "Incorrect!"
+    end
+  end
+end
